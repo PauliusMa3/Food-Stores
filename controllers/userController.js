f2f23816e6879796709bf129d854f8b341892743
Paulius <paulius@LAPTOP-A8OOU6R0.localdomain>
2019-09-19 11:57:38 +0300

    Updated Node Dependencies

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,5 @@
 const User = require("../models/User");
-const promisify = require("es6-promisify");
+const {promisify} = require("es6-promisify");
 
 const { validationResult } = require("express-validator");
 exports.login = (req, res) => {
